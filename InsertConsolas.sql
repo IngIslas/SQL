6a38cc11da3cdae84515d87274324b5a201a5c94
javier cruz islas <jv.cruz@mavi.mx>
2022-09-27 14:22:12 -0500

    Feature: Agregar Carga inicial de consolas

---
+++ InsertConsolas.sql
@@ -0,0 +1,15 @@
+
+
+--Carga de Consolas 
+INSERT INTO Consola (Nombre) Values ('Play Station 1')
+INSERT INTO Consola (Nombre) Values ('Play Station 2')
+INSERT INTO Consola (Nombre) Values ('Play Station Portatil')
+INSERT INTO Consola (Nombre) Values ('Play Station 3')
+INSERT INTO Consola (Nombre) Values ('X Box Clasica')
+INSERT INTO Consola (Nombre) Values ('X Box-360')
+INSERT INTO Consola (Nombre) Values ('PC')
+INSERT INTO Consola (Nombre) Values ('Wii')
+INSERT INTO Consola (Nombre) Values ('Nintengo switch')
+INSERT INTO Consola (Nombre) Values ('GameBoy')
+
+
